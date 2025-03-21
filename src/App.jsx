7bb65a70898eed6e3b3d96c159249ddb859e013e
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-03-21 12:20:31 +0100

    add: milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [politicians, setPoliticians] = useState([])
+  async function getList() {
+    try {
+      const response = await fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
+      const data = await response.json()
+      setPoliticians(data)
+      console.log(data)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    getList();
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>Lista Politici</h1>
+        <div className="row">
+          {politicians && politicians.map((p, id) =>
+            <div className="col" key={id}>
+              <h3>{p.name}</h3>
+              <img src={p.image} alt="" />
+              <span><strong>{p.position}</strong></span>
+              <p>{p.biography}</p>
+            </div>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
