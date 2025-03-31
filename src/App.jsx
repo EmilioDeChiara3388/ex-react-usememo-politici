d0ab7f1e31a6c4313ea7956bc8aee9f1a1880a18
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-03-31 12:07:53 +0200

    add: milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react"
 function App() {
 
   const [politicians, setPoliticians] = useState([])
+  const [search, setSearch] = useState("")
+
   async function getList() {
     try {
       const response = await fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
@@ -18,12 +20,24 @@ function App() {
     getList();
   }, [])
 
+  const filteredResults = search ? politicians.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()) ||
+    p.biography.toLowerCase().includes(search.toLowerCase())
+  ) : politicians;
+
   return (
     <>
       <div className="container">
         <h1>Lista Politici</h1>
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Cerca per nome o biografia..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
         <div className="row">
-          {politicians && politicians.map((p, id) =>
+          {filteredResults && filteredResults.map((p, id) =>
             <div className="col" key={id}>
               <h3>{p.name}</h3>
               <img src={p.image} alt="" />
